6eb4c59614fa32c3fdae1c0a0cc43bb788ce66d1
Flllyi <tua04072@gmail.com>
2020-09-25 17:58:58 -0400

    commit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,24 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
+use App\Models\post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function show($slug)
-    {
-        $post= \DB::table('posts')->where('slug', $slug)->first();
+    {   return view('post', [
+            'post'=> Post::where('slug', $slug)->firstOrFail()
 
-            // $posts = [
-            //     'my-first-post' => 'Hello, this is my first post',
-            //     'my-second-post' => 'Hello, this is my second post'
-            // ];
-
-
-            return view('post', [
-                'post' => $post
-            ]);
+        ]);
 
     }
 }
