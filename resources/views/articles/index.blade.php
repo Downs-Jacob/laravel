3e6350159d5a77c40cb74b38e4eb0151d4f3daaa
Flllyi <tua04072@gmail.com>
2020-10-20 18:32:28 -0400

    master

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,7 +3,7 @@
 @section ('content')
 <div id="wrapper">
 	<div id="page" class="container">
-        @foreach ($articles as $article)
+        @forelse ($articles as $article)
             <div class='content'>
                 <div class='title'>
                     <h2>
@@ -17,7 +17,9 @@
                 </p>
                 {!! $article->excerpt !!}
             </div>
-        @endforeach
+            @empty
+                <p> No Relevant Articles Yet!</p>
+        @endforelse
     </div>
 </div>
 @endsection
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,9 +5,21 @@
 	<div id="page" class="container">
 		<div id="content">
 			<div class="title">
-				<h2>{{ $article->title}}</h2
-			<p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
-			{{ $article->body}}
+                <h2>{{ $article->title}}</h2>
+            </div>
+			<p>
+                <img
+                    src="/images/banner.jpg"
+                    alt=""
+                    class="image image-full"
+                    />
+            </p>
+            {{ $article->body}}
+            <p>
+                @foreach ($article->tags as $tag)
+                    <a href="{{ route('articles.index', ['tag' => $tag->name])}}">{{ $tag->name }}</a>
+                @endforeach
+            </p>
 		</div>
 		</div>
 	</div>
